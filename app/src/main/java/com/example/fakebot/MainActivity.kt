b7d9c387ca2936380c7854ad8ecefa8315ba818d
luisnicolasss <86482520+luisnicolasss@users.noreply.github.com>
2023-02-14 12:28:56 -0300

    Views y adapter

--- app/src/main/java/com/example/fakebot/MainActivity.kt
+++ app/src/main/java/com/example/fakebot/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.fakebot
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.fakebot.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.chatRecycler.layoutManager = LinearLayoutManager(this)
+
+        val adapter = ChatAdapter()
+        binding.chatRecycler.adapter = adapter
     }
 }
